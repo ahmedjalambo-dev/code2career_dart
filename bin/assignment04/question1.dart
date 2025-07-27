4d4f3c3eea07613755b103b3082e5b1b43dca7c3
ahmedjalambo-dev <ahmedjalambo,dev@gmail.com>
2025-07-27 21:58:43 +0300

    some fromatting

--- bin/assignment04/question1.dart
+++ bin/assignment04/question1.dart
@@ -1,29 +1,8 @@
 void main(List<String> args) {
   greet('Layla');
   greet('Ali', 'Welcome');
-
-  registerUser(username: 'ahmed_jalambo', email: 'ahmedjalambo.dev@gmail.com');
-
-  printItem(['Apple', 'Banana', 'Grapes'], title: 'Fruits');
 }
 
-/// question 1
 greet(String name, [String greeting = 'Hello']) {
   print('$greeting, $name!');
-}
-
-/// question 2
-registerUser({required username, required email}) {
-  print('User $username registered with email $email');
-}
-
-/// question 3
-/// 
-
-/// question 4
-printItem(List<String> items, {String title = 'Item List'}) {
-  print(title);
-  for (var i = 0; i < items.length; i++) {
-    print('${i + 1}. ${items[i]}');
-  }
 }
